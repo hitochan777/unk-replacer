72d9a5b1c68d957efd531d6130d164e1ded10b2a
Hitoshi Otsuki <ilikestars55@yahoo.co.jp>
2017-01-31 19:41:20 +0900

    Add output option to adjust_alignment.py

--- bin/adjust_align.py
+++ bin/adjust_align.py
@@ -14,16 +14,17 @@ def main(args=None):
     parser.add_argument('second_src', type=str, help='Path to p2 source file')
     parser.add_argument('second_tgt', type=str, help='Path to p2 source file')
     parser.add_argument('alignment', type=str, help='Path to word alignment file to p1')
+    parser.add_argument('output', type=str, help='Path to output')
 
     options = parser.parse_args(args)
 
     with open(options.first_src) as first_src, open(options.first_tgt) as first_tgt, \
         open(options.second_src) as second_src, open(options.second_tgt) as second_tgt, \
-        open(options.alignment) as alignments:
+        open(options.alignment) as alignments, open(options.output, "w") as output:
 
         for sb, tb, sa, ta, align in zip(first_src, first_tgt, second_src, second_tgt, alignments):
             new_align_str = Alignment.get_adjusted_alignment(sb.strip().split(' '), tb.strip().split(' '), sa.strip().split(' '), ta.strip().split(' '), align.strip())
-            print(new_align_str)
+            print(new_align_str, file=output)
 
 if __name__ == "__main__":
     main()
